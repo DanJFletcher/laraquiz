36dc0fe15cd9fdd04ff8ac9682cd47d1f983c87b
DanJFletcher <dannyjfletcher@outlook.com>
2017-02-13 11:43:08 -0500

    Configure database name based on APP_ENV for sqlite

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => database_path($database),
             'prefix' => '',
         ],
 

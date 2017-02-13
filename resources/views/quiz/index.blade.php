b83d520c0eccebac4130322d2194b3a218ddae42
DanJFletcher <dannyjfletcher@outlook.com>
2017-02-13 16:44:35 -0500

    Add edit and show buttons to quiz index view

--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -14,6 +14,7 @@
                 <td>ID</td>
                 <td>Title</td>
                 <td>Owner</td>
+                <td>Action</td>
             </tr>
         </thead>
         <tbody>
@@ -22,7 +23,19 @@
             <tr>
                 <td>{{ $quiz->id }}</td>
                 <td>{{ $quiz->title }}</td>
-                <td>{{ $quiz->user->name }}
+                <td>{{ $quiz->user->name }}</td>
+                <td>
+
+                <!-- delete the quiz (uses the destroy method DESTROY /quiz/{id} -->
+                <!-- we will add this later since its a little more complicated than the other two buttons -->
+
+                <!-- show the quiz (uses the show method found at GET /quizs/{id} -->
+                <a class="btn btn-small btn-success" href="{{ URL::to('quiz/' . $quiz->id) }}">Show this quiz</a>
+
+                <!-- edit this quiz (uses the edit method found at GET /quizs/{id}/edit -->
+                <a class="btn btn-small btn-info" href="{{ URL::to('quiz/' . $quiz->id . '/edit') }}">Edit this quiz</a>
+
+            </td>
             </tr>
         @endforeach
         </tbody>

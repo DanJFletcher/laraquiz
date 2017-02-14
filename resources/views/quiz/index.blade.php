154094428e608f0c0755173096980fab2cff471a
DanJFletcher <dannyjfletcher@outlook.com>
2017-02-13 23:03:31 -0500

    Add delete button

--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -27,7 +27,10 @@
                 <td>
 
                 <!-- delete the quiz (uses the destroy method DESTROY /quiz/{id} -->
-                <!-- we will add this later since its a little more complicated than the other two buttons -->
+                {{ Form::open(array('url' => 'quiz/' . $quiz->id, 'class' => 'pull-right')) }}
+                    {{ Form::hidden('_method', 'DELETE') }}
+                    {{ Form::submit('Delete this Quiz', array('class' => 'btn btn-warning')) }}
+                {{ Form::close() }}
 
                 <!-- show the quiz (uses the show method found at GET /quizs/{id} -->
                 <a class="btn btn-small btn-success" href="{{ URL::to('quiz/' . $quiz->id) }}">Show this quiz</a>

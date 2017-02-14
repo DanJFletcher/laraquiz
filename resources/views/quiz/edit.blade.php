ab7006ec610a864bbad7021dbf7d8dd94a11b4f0
DanJFletcher <dannyjfletcher@outlook.com>
2017-02-14 17:09:22 -0500

    Add quiz.edit view

---
+++ resources/views/quiz/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <!-- if there are creation errors, they will show here -->
+        @if (isset($errors))
+            {{ HTML::ul($errors->all()) }}
+        @endif
+
+        {{ Form::open(['url' => 'quiz/' . $quiz->id]) }}
+
+        <div class="form-group">
+            {{ Form::label('title', 'Quiz Title') }}
+            {{ Form::text('title', Input::old('title'), array('class' => 'form-control')) }}
+        </div>
+
+        {{ Form::hidden('user_id', Auth::user()->id) }}
+        {{ Form::hidden('_method', 'PUT') }}
+
+        {{ Form::submit('Create the Quiz!', array('class' => 'btn btn-primary')) }}
+
+        {{ Form::close() }}
+    </div>
+
+
+
+@endsection()

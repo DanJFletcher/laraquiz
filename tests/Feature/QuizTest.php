52166dc3377ff5b44303c372598277292c2ef3c0
DanJFletcher <dannyjfletcher@outlook.com>
2017-02-14 19:07:18 -0500

    Add edge cases to quiz test

--- tests/Feature/QuizTest.php
+++ tests/Feature/QuizTest.php
@@ -14,11 +14,13 @@ class QuizTest extends TestCase
 {
 
     /**
-     * Display a list of quizzes
+     * Test index if authed
+     *
+     * Display a list of quizzes if user is authenticated
      *
      * @return void
      */
-    public function testIndex()
+    public function testIndexIfAuthed()
     {
         // Create user
         $user = factory(User::class)->create();
@@ -27,6 +29,54 @@ class QuizTest extends TestCase
         
         $response
             ->assertStatus(200)
+            ->assertViewHas('quizzes');
+    }
+
+    /**
+     * Test index if not authed
+     * 
+     * Redirect to login if user is not authenticated
+     *
+     * @return void
+     */
+    public function testIndexIfNotAuthed()
+    {
+        $response = $this->get('quiz');
+
+        $response
+            ->assertStatus(302)
+
+            // If not authed users should be redirected to /login
+            ->assertRedirect('login');
+    }
+
+    /**
+     * Test index no quizzes
+     * 
+     * Not sure
+     *
+     * @return void
+     */
+    public function testIndexIfNoQuizzes()
+    {
+        // Create authed user
+        $user = factory(User::class)->create();
+
+        // Clear DB of quizzes
+        $quizzes = Quiz::all();
+        $quizzes->each->delete();
+
+        // we shouldn't see any quizzes in the database
+        $this->assertTrue(count(Quiz::all()) === 0);
+
+        // no quizzes in database but we should still
+        // be able to reach the view
+        $response = $this->actingAs($user)->get('quiz');
+
+        $response
+            ->assertStatus(200)
+
+            // The view should still have a quizzes collection
             ->assertViewHas('quizzes');
     }
 
@@ -71,11 +121,11 @@ class QuizTest extends TestCase
     }
 
     /**
-     * Show a quiz.
+     * Show a quiz if quizzes exist
      *
      * @return void
      */
-    public function testShow()
+    public function testShowIfQuizzesExist()
     {
         // Create and store a new quiz
         $quiz = factory(Quiz::class)->create();
@@ -99,6 +149,34 @@ class QuizTest extends TestCase
         $response
             ->assertStatus(200)
             ->assertViewHas('quiz');
+    }
+
+    /**
+     * Show quiz if quizzes don't exist
+     *
+     * Should return 404 not found if there are no quizzes in database
+     *
+     * @return void
+     */
+    public function testShowQuizIfQuizzesDontExist()
+    {
+        // Create authed user
+        $user = factory(User::class)->create();
+
+        // Clear DB of quizzes
+        $quizzes = Quiz::all();
+        $quizzes->each->delete();
+
+        // we shouldn't see any quizzes in the database
+        $this->assertTrue(count(Quiz::all()) === 0);
+
+        // Call the show route with id that shouldn't exist
+        $response = $this->actingAs($user)->call('GET', 'quiz/1');
+
+        $response
+            ->assertStatus(404);
+
+
     }
 
     /**

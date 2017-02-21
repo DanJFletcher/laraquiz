f5d73d4e7cbd00ead48996c4570c6ca8c1ace256
Dan Fletcher <dannyjfletcher@outlook.com>
2017-02-20 20:28:27 -0500

    Show all questions belonging to a quiz

--- resources/views/quiz/show.blade.php
+++ resources/views/quiz/show.blade.php
@@ -3,5 +3,10 @@
 @section('content')
     <div class="container">
         <h1>{{ $quiz->title }}</h1>
+        <ul>
+            @foreach($quiz->questions as $question)
+                <li>{{ $question->text }}</li>
+            @endforeach
+        </ul>
     </div>
 @endsection

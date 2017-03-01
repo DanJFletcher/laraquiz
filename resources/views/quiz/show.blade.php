55b055d191c1b2e7b371e58091ad1119bcd13235
Dan Fletcher <dannyjfletcher@outlook.com>
2017-03-01 11:06:51 -0500

    Add message when no questions belong to quiz

--- resources/views/quiz/show.blade.php
+++ resources/views/quiz/show.blade.php
@@ -3,11 +3,15 @@
 @section('content')
     <div class="container">
         <h1>{{ $quiz->title }}</h1>
-        <ul>
-            @foreach($quiz->questions as $question)
-                <li>{{ $question->text }}</li>
-            @endforeach
-        </ul>
+        @if(count($quiz->questions) > 0)
+            <ul>
+                @foreach($quiz->questions as $question)
+                    <li>{{ $question->text }}</li>
+                @endforeach
+            </ul>
+        @else
+            <p>No questions in this quiz yet.</p>
+        @endif
         <a 
             href="{{ route('question.create', ['id' => $quiz->id]) }}"
             class="btn btn-primary"

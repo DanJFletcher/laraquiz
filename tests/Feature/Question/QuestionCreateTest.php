15dba924aecea7ebd5806d9d019c00e821ae923c
Dan Fletcher <dannyjfletcher@outlook.com>
2017-03-01 10:31:45 -0500

    Add test for question.create

---
+++ tests/Feature/Question/QuestionCreateTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Quiz;
+use App\User;
+
+class QuestionCreateTest extends TestCase
+{
+    /**
+     * Test creating a quesiont
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        // Create authed user
+        $user = factory(User::class)->create();
+
+        // Create quiz to add question to
+        $quiz = factory(\App\Quiz::class)->create();
+
+        $response = $this->actingAs($user)->get("question/create?id={$quiz->id}");
+
+        $data = $response->getOriginalContent()->getData();
+
+        $response
+            ->assertStatus(200)
+            
+            // The view should have an id
+            ->assertViewHas('id');
+
+            // And it should match the current quiz id
+            $this->assertTrue($data['id'] == $quiz->id);
+    }
+}

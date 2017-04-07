d2f6b6461846e2cf46b71925d310973c658cfd7a
DanJFletcher <dannyjfletcher@outlook.com>
2017-04-07 11:14:35 -0400

    Return with erros if quiz title is null on update

--- tests/Feature/Quiz/TestUpdateTest.php
+++ tests/Feature/Quiz/TestUpdateTest.php
@@ -33,7 +33,25 @@ class QuizUpdateTest extends TestCase
         $response
             ->assertStatus(302)
             ->assertRedirect('quiz');
-            
+
         $this->assertDatabaseHas('quizzes', ['title' => 'Update Quiz']);
+    }
+
+    /** @test */
+    public function passing_null_as_quiz_title_should_return_with_error()
+    {
+        // Create user and quiz
+        $user = factory(User::class)->create();
+        $quiz = factory(Quiz::class)->create();
+
+        // Send a quiz to be stored in database
+        $response = $this->actingAs($user)->call('PUT', 'quiz/'.$quiz->id,
+            array(
+                '_token' => csrf_token()
+            ));
+
+        $response
+            ->assertStatus(302)
+            ->assertSessionHas(['errors']);
     }
 }
